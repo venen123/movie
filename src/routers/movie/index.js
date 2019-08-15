dc2d512731aad4cb26b9be41f1955f534b962d4e
7c0_100006781762_venen <7c0_100006781762_venen@git.cloud.tencent.com>
2019-08-15 18:38:44 +0800

    createcomponents

---
+++ src/routers/movie/index.js
@@ -0,0 +1,30 @@
+export default { 
+  path: '/movie',
+  component: () =>
+    import ('@/views/movie'),
+  children: [{
+      path: 'city',
+      component: () =>
+        import ('@/components/city')
+    },
+    {
+      path: 'nowplaying',
+      component: () =>
+        import ('@/components/nowplaying')
+    },
+    {
+      path: 'comingsoon',
+      component: () =>
+        import ('@/components/comingsoon')
+    },
+    {
+      path: 'search',
+      component: () =>
+        import ('@/components/search')
+    },
+    {
+      path:'/movie',
+      redirect: '/movie/nowplaying'
+    }
+  ]
+}
---
+++ src/views/cinema/index.vue
@@ -0,0 +1,39 @@
+
+<template>
+  <div id="main">
+    <Header title="影院"></Header>
+    <div id="content">
+      <div class="cinema_menu">
+				<div class="city_switch">
+					全城 <i class="iconfont icon-lower-triangle"></i>
+				</div>
+				<div class="brand_swtich">
+					品牌 <i class="iconfont icon-lower-triangle"></i>
+				</div>
+				<div class="feature_switch">
+					特色 <i class="iconfont icon-lower-triangle"></i>
+				</div>
+			</div>
+     <Cilist></Cilist>
+    </div>
+    <Footer></Footer>
+  </div>
+</template>
+
+<script>
+import Header from '@/components/header'
+import Footer from '@/components/footer'
+import Cilist from '@/components/cilist'
+export default {
+    name:'cinema',
+    components:{
+    Header,
+    Footer,
+    Cilist
+  }
+}
+</script>
+
+<style scoped>
+#content .cinema_menu{ width: 100%; height: 45px; border-bottom:1px solid #e6e6e6; display: flex; justify-content:space-around; align-items:center; background:white;}
+</style>
